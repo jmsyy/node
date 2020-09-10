67ad4c2a9048bc287968c6ab6430ba9819f3446c
jmsyy <jmsyuyang777@gmail.com>
2020-09-10 10:39:47 +0800

    add postger SQL

--- db/postgres.js
+++ db/postgres.js
@@ -6,3 +6,48 @@ db.connect( (err, client) => {
   console.log(`connect to database`, db.database);
   db.end();
 } )
+
+// define table
+db.query(`
+  CERATE TABLE IF NOT EXISTS  example(
+    id SERTAL,
+    PRIMARY KEY (id),
+    body text
+  );
+`, (err, result) => {
+  if(err){ throw  err}
+  console.log(`Created table example`);
+  db.end();
+});
+
+// insert data
+
+const body = 'hello postgreSql'
+db.query(`
+  INSERT INTO example (body) VALUES ('${body}')
+  RETURNING id
+`, (err, result) => {
+  if(err) {throw err}
+  const id = result.rows[0].id;
+  console.log(`Insert row with id %s`, id);
+});
+
+// update data
+
+const id = 1, updateTxt = 'hello node postgreSql';
+db.query(`
+  UPDATE example SET (body) = ('${body}') WHERE id=${id};
+`, (err, result) => {
+  if (err) {
+    throw err
+  }
+  console.log(`Update %s rows`, result.rowCount);
+})
+
+// query data
+db.query(`
+  SELECT * FROM example ORDER BY id
+`, (err, result) =>{
+  if(err) throw err;
+  console.log(result.rows);
+});
